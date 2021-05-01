6425867130aafebbb6d40b4dca93e56a38dc5f8e
MichalMaciszka <t-ja15@wp.pl>
2021-05-01 14:43:33 +0200

    imgs

--- test.py
+++ test.py
@@ -42,7 +42,7 @@ def get_images_urls(array, page_url):
             urls.append(img_url)
     return urls
 
-def download(url, pathname):
+def download(url, pathname, it):
     # if path doesn't exist, make that path dir
     if not os.path.isdir(pathname):
         os.makedirs(pathname)
@@ -51,14 +51,13 @@ def download(url, pathname):
     # get the total file size
     file_size = int(response.headers.get("Content-Length", 0))
     # get the file name
-    filename = os.path.join(pathname, url.split("/")[-1])
-    #filename = os.path.join(pathname, str(it) + ".jpg")
+    #filename = os.path.join(pathname, url.split("/")[-1])
+    filename = os.path.join(pathname, str(it))
     # progress bar, changing the unit to bytes instead of iteration (default by tqdm)
     progress = tqdm(response.iter_content(1024), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
-    time.sleep(10)
     with open(filename, "wb") as f:
         for data in progress:
-            time.sleep(10)
+            time.sleep(2)
             # write data read to the file
             f.write(data)
             # update the progress bar manually
@@ -68,7 +67,9 @@ def main():
     url = "https://www.olx.pl/elektronika/q-pendrive/"
     path = "imgs"
     images= get_list_of_images(url)
+    no = 1
     for img in images:
-        download(img, path)
+        download(img, path, no)
+        no += 1
 
 main()
