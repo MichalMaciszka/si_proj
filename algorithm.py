64dbc2240fb5d1638ec3da67cfc067609a9dd0a8
KonradSuszko <34981439+KonradSuszko@users.noreply.github.com>
2021-06-02 14:06:15 +0200

    Different_parameters

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,30 @@
+import random
+
+
+def pre_clastering(offers, hist, text):
+    val = 0.99*hist - 0.04*text
+    original = offers.copy()
+    print("pre-clastering...")
+    result = []
+    while len(original) > 0:
+        r = random.choice(original)
+        original.remove(r)
+        tmp = [r]
+        for x in original.copy():
+            a, b = r.compare_offer(x)
+            s = (a*hist + b*text)/(hist + text)
+            if s >= val:
+                print(s)
+                print(r, x)
+                tmp.append(x)
+                original.remove(x)
+        result.append(tmp)
+    print()
+    return result
+
+
+
+
 def construct_array(claster_list, hist, text):
     res = []
     for i in range(len(claster_list)):
@@ -9,6 +36,7 @@ def construct_array(claster_list, hist, text):
                 tmp.append(-1)
         res.append(tmp)
     return res
+
 
 def get_max_i_j(array):
     val = 0
@@ -22,12 +50,27 @@ def get_max_i_j(array):
                 max_j = j
     return max_i, max_j
 
+
 def algorithm(clasters, hist, text, progressBar, num):
     c = clasters.copy()
-    while len(c) > num:
+    l = len(c) / num
+    print("l = ", l)
+    result = []
+    # while len(c) > num:
+    while len(result) < num - 1 and len(c) > 5:
         p = construct_array(c, hist, text)
         x, y = get_max_i_j(p)
-        c[x].list.extend(c[y].list)
+        c[x].list.extend(c[y].list.copy())
+        print(p[x][y])
         c.pop(y)
-        progressBar.setProperty("value", 40 + (num/len(c) * 50))
-    return c
+        if len(c[x].list) >= l:
+            result.append(c[x])
+            c.pop(x)
+        progressBar.setProperty("value", 40 + (num / len(c) * 50))
+    tmp = c[0]
+    c.pop(0)
+    for e in c:
+        tmp.list.extend(e.list)
+    # result.extend(c)
+    result.append(tmp)
+    return result
